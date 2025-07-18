5855cd5356587c50246026d99be9c44490aa1f30
qm <qm@asuround.home.lan>
2025-07-18 18:37:07 +0200

    feat: remove diag icons in scrollbar

--- lua/plugins/ui/editor/scrollview-nvim.lua
+++ lua/plugins/ui/editor/scrollview-nvim.lua
@@ -1,12 +1,17 @@
 return
 {
     'dstein64/nvim-scrollview',
-    desc="make scrolling smoother",
+    enabled = true,
 
     config = function()
         require("scrollview").setup({
-             diagnostics_severities = {}
+            excluded_filetypes = {},
+            --signs_on_startup = {"search", "diagnostics" }, --"cursor"
+            signs_on_startup = {},
+            diagnostics_severities = {vim.diagnostic.severity.ERROR},
         })
+        vim.g.scrollview_signs_scrollbar_overlap = 'over'
+        vim.g.scrollview_signs_max_per_row = 1
 
     end,
 

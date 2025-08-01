51d8463a2bd6c0a4731ea49f36bc2aefa2cafc9b
qm <qm@asuround.home.lan>
2025-08-01 23:12:02 +0200

    feat: better gutter gitsigns

--- lua/plugins/ui/gitsign.lua
+++ lua/plugins/ui/gitsign.lua
@@ -3,48 +3,49 @@ return
     "lewis6991/gitsigns.nvim",
     enabled = true,
 
-    require('gitsigns').setup {
-        watch_gitdir = { follow_files = true },
+    config = function ()
+        require('gitsigns').setup {
+            watch_gitdir = { follow_files = true },
 
-        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        signs = {
-            add          = { text = '┃' },     --┃
-            change       = { text = '┃' },
-            delete       = { text = '_' },
-            topdelete    = { text = '‾' },
-            changedelete = { text = '~' },
-            untracked    = { text = '┆' },
-        },
+            signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+            numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+            linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+            signs = {
+                add          = { text = '┃' },  -- ┃
+                change       = { text = '┣' },
+                delete       = { text = '×' },  -- _
+                topdelete    = { text = '×' },  -- ‾
+                changedelete = { text = '×' },  --◇ *∗¤× ∙               untracked    = { text = '¦' },
+            },
 
-        signs_staged_enable = false,
-        signs_staged = {
-            add          = { text = '┃' },
-            change       = { text = '┃' },
-            delete       = { text = '_' },
-            topdelete    = { text = '‾' },
-            changedelete = { text = '~' },
-            untracked    = { text = '┆' },
-        },
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
-            virt_text = true,
-            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            signs_staged_enable = false,
+            signs_staged = {
+                add          = { text = '┃' },
+                change       = { text = '┃' },
+                delete       = { text = '┻' },
+                topdelete    = { text = '┳' },
+                changedelete = { text = '┣' },
+                untracked    = { text = '¦' },
+            },
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
                 delay = 1000,
-            ignore_whitespace = false,
-            virt_text_priority = 100,
-            use_focus = true,
-        },
-        current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-        preview_config               = {
-            -- Options passed to nvim_open_win
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
+            },
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+            preview_config               = {
+                -- Options passed to nvim_open_win
                 style = 'minimal',
                 relative = 'cursor',
                 row = 0,
                 col = 1
-        },
-        max_file_length = 40000, -- Disable if file is longer than this (in lines)
-    }
+            },
+            max_file_length = 40000, -- Disable if file is longer than this (in lines)
+        }
+    end
 }

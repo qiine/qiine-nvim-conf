cbeafe11c789d4f11ec60714810aae27e38a5ddf
qm <qm@asuround.home.lan>
2025-08-05 22:59:57 +0200

    chore: cleanup tests dir

--- tests/debug_init.lua
+++ tests/debug_init.lua
@@ -1,5 +1,10 @@
+--Use this file as a minimal test config
+--type nvim -u ~/Personal/dotfiles/User/nvim/tests/debug_init.lua
+
 vim.cmd("aunmenu PopUp")
  vim.cmd [[
   anoremenu PopUp.Inspect     <cmd>Inspect<CR>
 ]]
+
+vim.opt.number         = true
 

569f6cb6e426c12a783fec7b3a3b8ea7658030ec
qm <qm@asuround.home.lan>
2025-08-11 14:32:57 +0200

    fix: surround with C-< works properly again

--- lua/modules/v-enveloppe.lua
+++ lua/modules/v-enveloppe.lua
@@ -14,7 +14,6 @@ function M.enveloppe_selection(schar, echar)
 end
 
 function M.setup()
-
     --paren
     vim.keymap.set("i", "<C-(>", '<esc>m`"_diwi(<esc>P`]a)``')
     vim.keymap.set("n", "<C-(>", 'm`"_diwi(<esc>P`]a)``' )
@@ -27,12 +26,12 @@ function M.setup()
     --chevron
     vim.keymap.set("i", '<C-<>', "<esc>mzdiwi<<esc>P`]a><esc>`zli",      { desc = "enveloppe word angled brackets" })
     vim.keymap.set("n", '<C-<>', "mzdiwi<<esc>P`]a><esc>`zl",            { desc = "enveloppe word angled brackets" })
-    vim.keymap.set("v", '<C-">', function() M.enveloppe_selection("<",">") end)
+    vim.keymap.set("v", '<C-<>', function() M.enveloppe_selection("<",">") end)
 
     --single quotes
     vim.keymap.set("i", "<C-'>", [[<esc>m`"zdiwi'<esc>"zP`]a'<esc>``li]], { desc = "Surround visual selection with char" })
     vim.keymap.set("n", "<C-'>", [[m`"zdiwi'<esc>"zP`]a'<esc>``l]], { desc = "Surround visual selection with char" })
-    vim.keymap.set("v", '<C-">', function() M.enveloppe_selection("'","'") end)
+    vim.keymap.set("v", "<C-'>", function() M.enveloppe_selection("'","'") end)
 
     --double quote
     vim.keymap.set("i", '<C-">', '<esc>m`"zdiwi"<esc>"zP`]a"<esc>``li', {desc = "Surround visual selection with char" })

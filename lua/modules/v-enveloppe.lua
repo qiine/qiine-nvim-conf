cd7d26da53d6d098d2117e31204afcf09bb29a25
qm <qm@asuround.home.lan>
2025-09-02 22:00:26 +0200

    feat: v-enveloppe use new algo

--- lua/modules/v-enveloppe.lua
+++ lua/modules/v-enveloppe.lua
@@ -3,50 +3,48 @@
 
 local M = {}
 
-
-function M.enveloppe_selection(schar, echar)
-    vim.cmd('norm! m`')  --save curso pos
-    vim.cmd('norm! "zd') --get and rem text
-    vim.cmd('norm! i'..schar)
-    vim.cmd('norm! "zP')
-    vim.cmd('norm! a'..echar)
-    vim.cmd('norm! ``')  --restore curso pos
+function M.enveloppe_select(schar, echar)
+    vim.cmd('norm! m`') -- save curso pos
+    vim.cmd('norm! ') -- update `>`< proper pos
+    vim.cmd('norm! `>a'..echar) -- "a" first, order matter
+    vim.cmd('norm! `<i'..schar)
+    vim.cmd('norm! ``') -- Restore curso pos
 end
 
 function M.setup()
     --paren
     vim.keymap.set("i", "<C-(>", '<esc>m`"_diwi(<esc>P`]a)``')
     vim.keymap.set("n", "<C-(>", 'm`"_diwi(<esc>P`]a)``' )
-    vim.keymap.set("v", '<C-(>', function() M.enveloppe_selection('(',')') end)
+    vim.keymap.set("x", '<C-(>', function() M.enveloppe_select('(',')') end)
 
     --vim.keymap.set("v", '<C-[>', function() M.enveloppe_selection('[',']') end)
 
-    vim.keymap.set("v", '<C-{>', function() M.enveloppe_selection('{','}') end)
+    vim.keymap.set("x", '<C-{>', function() M.enveloppe_select('{','}') end)
 
     --chevron
     vim.keymap.set("i", '<C-<>', "<esc>mzdiwi<<esc>P`]a><esc>`zli",      { desc = "enveloppe word angled brackets" })
     vim.keymap.set("n", '<C-<>', "mzdiwi<<esc>P`]a><esc>`zl",            { desc = "enveloppe word angled brackets" })
-    vim.keymap.set("v", '<C-<>', function() M.enveloppe_selection("<",">") end)
+    vim.keymap.set("x", '<C-<>', function() M.enveloppe_select("<",">") end)
 
     --single quotes
     vim.keymap.set("i", "<C-'>", [[<esc>m`"zdiwi'<esc>"zP`]a'<esc>``li]], { desc = "Surround visual selection with char" })
     vim.keymap.set("n", "<C-'>", [[m`"zdiwi'<esc>"zP`]a'<esc>``l]], { desc = "Surround visual selection with char" })
-    vim.keymap.set("v", "<C-'>", function() M.enveloppe_selection("'","'") end)
+    vim.keymap.set("x", "<C-'>", function() M.enveloppe_select("'","'") end)
 
     --double quote
     vim.keymap.set("i", '<C-">', '<esc>m`"zdiwi"<esc>"zP`]a"<esc>``li', {desc = "Surround visual selection with char" })
     vim.keymap.set("n", '<C-">', 'm`"zdiwi"<esc>"zP`]a"<esc>``l',       {desc = "Surround visual selection with char" })
-    vim.keymap.set("v", '<C-">', function() M.enveloppe_selection('"','"') end)
+    vim.keymap.set("x", '<C-">', function() M.enveloppe_select('"','"') end)
 
-    vim.keymap.set("v", '<C-|>', function() M.enveloppe_selection('|','|') end)
+    vim.keymap.set("x", '<C-|>', function() M.enveloppe_select('|','|') end)
 
     --*
-    vim.keymap.set("v", '<C-*>', function() M.enveloppe_selection('*','*') end)
+    vim.keymap.set("x", '<C-*>', function() M.enveloppe_select('*','*') end)
 
-    vim.keymap.set("v", '<C-->', function() M.enveloppe_selection('-','-') end)
+    vim.keymap.set("x", '<C-->', function() M.enveloppe_select('-','-') end)
 
     -- /
-    vim.keymap.set("v", '<C-/>', function() M.enveloppe_selection('/','/') end)
+    vim.keymap.set("x", '<C-/>', function() M.enveloppe_select('/','/') end)
 
 end
 

f9c7fc689f84f7653e9944b1d4c5286cf567ca12
qm <qm@asuround.home.lan>
2025-09-15 16:59:18 +0200

    feat: tweak csvview settings

--- lua/plugins/editing/csvview.lua
+++ lua/plugins/editing/csvview.lua
@@ -6,29 +6,40 @@ return
 
     config = function ()
         require("csvview").setup({
-            parser = { comments = { "#", "//" } },
+            parser = {
+                delimiter = {
+                    ft = {
+                        csv = "|",
+                        tsv = "\t",
+                    },
+                comments = { "#", "//" } },
+            },
+
             view = {
+                spacing = 0,
                 display_mode = "border",
                 header_lnum = 1, -- true
                 sticky_header = {
-                    enabled = true,
+                    enabled   = true,
                     separator = "─",
                 },
             },
+
             keymaps = {
                 -- Text objects for selecting fields
                 textobject_field_inner = { "if", mode = { "o", "x" } },
                 textobject_field_outer = { "af", mode = { "o", "x" } },
+
                 -- Excel-like navigation:
-                -- Use <Tab> and <S-Tab> to move horizontally between fields.
-                -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-                -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-                jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
-                jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
-                jump_next_row = { "<Enter>", mode = { "n", "v" } },
-                jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+                jump_next_field_end = { "<Tab>", mode = { "i", "n", "v" } },
+                jump_prev_field_end = { "<S-Tab>", mode = { "i", "n", "v" } },
+                jump_next_row = { "<Enter>", mode = { "i", "n", "v" } },
+                jump_prev_row = { "<S-Enter>", mode = { "i", "n", "v" } },
             },
         })
+
+        -- highlight header row
+        vim.api.nvim_set_hl(0, "CsvViewHeaderLine", { fg = "#595959", bg = "#D1D1D1", bold = false })
 
         vim.api.nvim_create_autocmd("BufEnter", {
             group = "UserAutoCmds",

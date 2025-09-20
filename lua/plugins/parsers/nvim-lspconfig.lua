ad7a8ae5911140da58db7e924f809edf21e37a48
qm <qm@asuround.home.lan>
2025-09-20 23:30:50 +0200

    feat(LSP): simplify LSP config

--- lua/plugins/parsers/nvim-lspconfig.lua
+++ lua/plugins/parsers/nvim-lspconfig.lua
@@ -2,12 +2,5 @@ return
 {
     "neovim/nvim-lspconfig",
     enabled = true,
-
-    dependencies = {"saghen/blink.cmp"},
-
-    config = function()
-        local lspconfig    = require('lspconfig')
-        local capabilities = require("blink.cmp").get_lsp_capabilities()
-    end
 }
 

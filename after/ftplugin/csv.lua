08a1edc6e752b208ed24c245796042be3d4badd8
qm <qm@asuround.home.lan>
2025-09-22 22:01:26 +0200

    feat: disable some keybind in csv files

---
+++ after/ftplugin/csv.lua
@@ -0,0 +1,10 @@
+
+vim.opt_local.signcolumn = "no"
+
+-- unbind Indent inc
+vim.keymap.set({"i","n","v"}, "<Tab>", "", {buffer=true})
+
+-- unbind Indent decrease
+vim.keymap.set({"i","n","v"}, "<S-Tab>", "", {buffer=true})
+
+

bdcffcdc6eac952faf57d79f4605fbc63b5b7493
qm <qm@asuround.home.lan>
2025-10-02 22:00:02 +0200

    feat: add is_bigfile buffer property to file above a size threshold

---
+++ filetype.lua
@@ -0,0 +1,13 @@
+
+vim.filetype.add {
+    pattern = {
+        ['.*'] = function(path, bufnr)
+            if path and vim.fn.getfsize(path) > 1024 * 1024 then
+                vim.b[bufnr].is_bigfile = true
+            end
+
+            return nil -- keep normal detection
+        end,
+    },
+}
+

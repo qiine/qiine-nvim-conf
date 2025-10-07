0b4420f61854d4c5f96fc267c495bea7ae00a05a
qm <qm@asuround.home.lan>
2025-10-07 18:31:21 +0200

    feat: clear line without deleting pipes in csv files

--- after/ftplugin/csv.lua
+++ after/ftplugin/csv.lua
@@ -7,6 +7,14 @@ vim.keymap.set({"i","n","v"}, "<Tab>", "", {buffer=true})
 -- Unbind indent decrease
 vim.keymap.set({"i","n","v"}, "<S-Tab>", "", {buffer=true})
 
+-- clear line
+
+vim.keymap.set({"i","n","v"}, "<S-BS>", function()
+    local crs_pos = vim.api.nvim_win_get_cursor(0)[1]
+    vim.cmd("silent! "..crs_pos..[[s/[^|]\+//g]])
+    vim.cmd("noh")
+end, {buffer=true})
+
 -- Trigger csv
 vim.cmd("CsvViewEnable")
 

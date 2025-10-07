e664cff78f5fb7aa11961e1cc05f7bea4ad03467
qm <qm@asuround.home.lan>
2025-10-07 16:22:51 +0200

    feat: tweak autopair

--- lua/plugins/editing/nvim-autopairs.lua
+++ lua/plugins/editing/nvim-autopairs.lua
@@ -1,13 +1,14 @@
 return
 {
     'windwp/nvim-autopairs',
+    enabled = true,
 
     event = "InsertEnter",
     config = function()
         require("nvim-autopairs").setup({
             enable_afterquote = false,
             map_cr = false,
-            map_bs = false,
+            map_bs = true,
         })
     end,
 

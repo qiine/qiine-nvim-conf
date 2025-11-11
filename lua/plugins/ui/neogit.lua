d90f07bdae57551e75ce13a6b667063470f2eb70
qiine <qm@asuround.home.lan>
2025-11-11 21:43:51 +0100

    feat: enabling neogit

--- lua/plugins/ui/neogit.lua
+++ lua/plugins/ui/neogit.lua
@@ -1,17 +1,33 @@
 return {
     "TimUntersberger/neogit",
-    enabled = false,
+    enabled = true,
     cmd = "Neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",         -- required
+        "sindrets/diffview.nvim",        -- optional - Diff integration
+
+        -- Only one of these is needed.
+        "ibhagwan/fzf-lua",              -- optional
+    },
 
     config = function()
         require("neogit").setup({
-            kind = "tab", -- opens neogit in a split
-            signs = {
-                -- { CLOSED, OPENED }
-                section = { "", "" },
-                item = { "", "" },
-                hunk = { "", "" },
+            kind = "floating", -- tab, split
+            floating = {
+                relative = "editor",
+                width = 0.85,
+                height = 0.8,
+                style = "minimal",
+                border = "rounded",
             },
+            -- signs = {
+            --     -- { CLOSED, OPENED }
+            --     section = { "", "" },
+            --     item = { "", "" },
+            --     hunk = { "", "" },
+            -- },
+            disable_context_highlighting = true,
+            graph_style = "unicode", -- ascii
             integrations = { diffview = true }, -- adds integration with diffview.nvim
         })
     end,

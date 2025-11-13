608abba24cad2adba51f5dd6c1c71769a9a67005
qiine <qm@asuround.home.lan>
2025-11-13 21:00:16 +0100

    feat: add floatwins utils

--- lua/utils/utils.lua
+++ lua/utils/utils.lua
@@ -1,4 +1,4 @@
--------------------------
+    -------------------------
 -- utils --
 -------------------------
 
@@ -272,6 +272,49 @@ end
 
 function M.create_file(name, path, opts)
 
+end
+
+
+
+-- ## [Windows]
+----------------------------------------------------------------------
+-- TODO
+---@param buf number?
+---@param enter boolean?
+---@param opts table?
+function M.fwin_open(buf, enter, opts)
+    buf   = buf and buf or 0
+    enter = enter and enter or true
+    opts  = opts or {}
+
+
+    local edw = { w = vim.o.columns, h = vim.o.lines }
+
+    local wsize = { -- relative size
+        w = math.floor(edw.w * (opts.wratio and opts.wratio or 0.9) ),
+        h = math.floor(edw.h * (opts.hratio and opts.hratio or 0.8) ),
+    }
+
+    local wopts = {
+        title     = opts.title and opts.title or vim.fn.expand("%:t"),
+        title_pos = opts.title_pos and opts.title_pos or "center",
+
+        relative  = "editor",
+        width     = wsize.w,
+        height    = wsize.h,
+        col       = math.floor((edw.w - wsize.w) / 2),
+        row       = math.floor((edw.h - wsize.h) / 2) - 1,
+
+        border    = opts.border and opts.border or "single",
+    }
+
+    -- if opts then
+    --     wopts = vim.tbl_deep_extend("force", wopts, opts)
+    -- end
+
+    local fwin = vim.api.nvim_open_win(buf, enter, wopts)
+
+    vim.api.nvim_set_option_value("winblend", 0, {win=fwin})
 end
 
 

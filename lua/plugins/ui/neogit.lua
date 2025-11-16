fb1602eb3f9a0b661b8b1b3802fd209bae8b0b5a
qiine <qm@asuround.home.lan>
2025-11-16 19:33:42 +0100

    feat: config neogit some more

--- lua/plugins/ui/neogit.lua
+++ lua/plugins/ui/neogit.lua
@@ -22,22 +22,44 @@ return
                 border = "rounded",
             },
             disable_signs = true,
-            -- signs = {
-            --     -- { CLOSED, OPENED }
-            --     section = { "", "" },
-            --     item = { "", "" },
-            --     hunk = { "", "" },
-            -- },
             disable_context_highlighting = true,
             graph_style = "unicode", -- ascii
             integrations = { diffview = true }, -- adds integration with diffview.nvim
             disable_hint = true,
+
+            status = {
+                recent_commit_count = 50,
+                mode_text = {
+                    M = "M",
+                    N = "new file",
+                    A = "A",
+                    D = "D",
+                    C = "copied",
+                    U = "updated",
+                    R = "R",
+                    DD = "unmerged",
+                    AU = "unmerged",
+                    UD = "unmerged",
+                    UA = "unmerged",
+                    DU = "unmerged",
+                    AA = "unmerged",
+                    UU = "unmerged",
+                },
+                mode_padding = 1,
+            },
+
+            -- Automatically show console if a command takes more than console_timeout milliseconds
+            auto_show_console = false,
+
             mappings = {
                 commit_editor = {
-                    ["<C-S-\13>"] = "Submit",
+                    ["<C-S-CR>"] = "Submit",
                     ["<C-w>"] = "Abort",
                     ["<C-p>"] = "PrevMessage",
                     ["<C-n>"] = "NextMessage",
+                },
+                commit_editor_I = {
+                    ["<C-S-CR>"] = "Submit",
                 },
             },
         })

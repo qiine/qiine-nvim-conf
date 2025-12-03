ce41809b8364c171896a5a47811a748684af576d
qiine <qm@asuround.home.lan>
2025-12-03 13:15:04 +0100

    feat(fs): file_open_surrounding now print curr file id and rank

--- lua/fs.lua
+++ lua/fs.lua
@@ -1,36 +1,13 @@
 
 -- Filesystem --
 
--- fs ut
+-- fs utils
 local U = {}
-
-function U.append_select_to_file()
-    vim.ui.input({prompt="Append selected to: ", default=vim.fn.getcwd(), completion="file"},
-    function(input)
-        if input == nil then vim.notify("Append canceled. ", vim.log.levels.INFO) return end
-
-        vim.cmd("norm! ")
-        local lines = vim.fn.getline("'<","'>")
-        vim.fn.writefile(lines, input, "a")
-    end)
-end
-
-function U.move_select_to_file()
-    vim.ui.input({prompt="Move selected to: ", default=vim.fn.getcwd(), completion="file"},
-    function(input)
-        if input == nil then vim.notify("Move canceled. ", vim.log.levels.INFO) return end
-
-        vim.cmd("norm! ")
-        local lines = vim.fn.getline("'<","'>")
-        vim.cmd("norm! gvd")
-        vim.fn.writefile(lines, input, "a")
-    end)
-end
 
 ---@param dirpath string
 ---@param ignore? table
 ---@return table
-function U.get_files_dir(dirpath, ignore)
+function U.dir_get_files(dirpath, ignore)
     ignore = ignore and ignore or {}
 
     local files = {}
@@ -50,20 +27,20 @@ end
 
 
 -- fs
-local M = {}
+local F = {}
 
-function M.append_select_to_file()
+function F.append_select_to_file()
     vim.ui.input({prompt="Append selected to: ", default=vim.fn.getcwd(), completion="file"},
     function(input)
         if input == nil then vim.notify("Append canceled. ", vim.log.levels.INFO) return end
 
-        vim.cmd("norm! ")
+       vim.cmd("norm! ")
         local lines = vim.fn.getline("'<","'>")
         vim.fn.writefile(lines, input, "a")
     end)
 end
 
-function M.move_select_to_file()
+function F.move_select_to_file()
     vim.ui.input({prompt="Move selected to: ", default=vim.fn.getcwd(), completion="file"},
     function(input)
         if input == nil then vim.notify("Move canceled. ", vim.log.levels.INFO) return end
@@ -75,9 +52,48 @@ function M.move_select_to_file()
     end)
 end
 
+-- nav
+function F.file_open_surrounding(reverse)
+    reverse = reverse or false
 
-M.utils = U
+    local cwd = vim.fn.getcwd()
+    local cfpath, cfdir = vim.fn.expand("%:p"), vim.fn.expand("%:p:h")
+
+    local searchdir = cfdir == cwd and cfdir or cwd -- prefering cwd allow more nav freedom
+
+    -- TODO FEAT allow to filter certain files like bin
+    local files = {}
+    for name, type in vim.fs.dir(searchdir) do
+        if type == "file" then table.insert(files, searchdir.."/"..name) end
+    end
+
+    -- find curr file index
+    table.sort(files)
+    local idx = nil
+    for i, file in ipairs(files) do
+        if file == cfpath then idx = i break end
+    end
+    if not idx then idx = 1 end -- in case curfile !cwd, fallback to first item
+
+    local nextfid
+    if reverse then
+        nextfid = math.min(idx+1, #files)
+    else
+        nextfid = math.max(idx-1, 1)
+    end
+    -- local previdx = (idx - 2) % #files + 1 -- wraparound
+    local nextf = files[nextfid]
+
+    if not nextf or nextf == cfpath then return end
+
+    local oldbuf = vim.api.nvim_get_current_buf()
+    vim.cmd("e! "..nextf)
+    vim.api.nvim_buf_delete(oldbuf, {force=true})
+    print("File: "..nextfid.."/"..#files)
+end
+
 
 --------
-return {M, U}
+F.utils = U
+return F
 

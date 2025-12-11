da70f9e699a63acf3a98ef0f885101a86cd839f4
qiine <qm@asuround.home.lan>
2025-12-11 18:31:51 +0100

    feat: turn most lua abbrev off for now, too many accidental triggering

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,10 +1,10 @@
 
 -- lua --
 
-vim.keymap.set("ia", "fn",  "function", {buffer=true})
-vim.keymap.set("ia", "loc", "local",    {buffer=true})
-vim.keymap.set("ia", "ret", "return",   {buffer=true})
-vim.keymap.set("ia", "req", "require",  {buffer=true})
+-- vim.keymap.set("ia", "fn",  "function", {buffer=true})
+-- vim.keymap.set("ia", "loc", "local",    {buffer=true})
+-- vim.keymap.set("ia", "ret", "return",   {buffer=true})
+-- vim.keymap.set("ia", "req", "require",  {buffer=true})
 
 vim.keymap.set("ia", "!=",  "~=",       {buffer=true})
 

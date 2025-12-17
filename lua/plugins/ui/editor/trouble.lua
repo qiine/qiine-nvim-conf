684225ed149cc00143f8745d284a372870bf718f
qiine <qm@asuround.home.lan>
2025-12-17 22:50:27 +0100

    feat(trouble): config further, trouble.nvim

--- lua/plugins/ui/editor/trouble.lua
+++ lua/plugins/ui/editor/trouble.lua
@@ -7,8 +7,48 @@ return {
 
     config = function()
         require("trouble").setup({
+            -- focus = false,
             warn_no_results = false,
             open_no_results = true,
+            auto_preview    = false,
+            follow    = false,
+            auto_jump = false,
+            restore   = false,
+
+            keys = {
+                r = "refresh",
+                R = "toggle_refresh",
+                q = "close",
+                ["<C-CR>"] = "jump_close",
+                ["<Tab>"] = {
+                    action = function()
+                        require("trouble").next()
+                        require("trouble").preview()
+                    end,
+                },
+                ["<S-Tab>"] = {
+                    action = function()
+                        require("trouble").prev()
+                        require("trouble").preview()
+                    end,
+                },
+                ["<CR>"] = "preview",
+                ["<M-z>"] = "fold_toggle",
+                ["<M-S-z>"] = "fold_toggle_recursive",
+                -- ["<M-q>"] = "quickfix",
+                s = { -- example of a custom action that toggles the severity
+                    action = function(view)
+                        local f = view:get_filter("severity")
+                        local severity = ((f and f.filter.severity or 0) + 1) % 5
+                        view:filter({ severity = severity }, {
+                            id = "severity",
+                            template = "{hl:Title}Filter:{hl} {severity}",
+                            del = severity == 0,
+                        })
+                    end,
+                    desc = "Toggle Severity Filter",
+                },
+            }
         })
     end,
 }

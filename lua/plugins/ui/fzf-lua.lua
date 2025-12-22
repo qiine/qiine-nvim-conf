c2b94cd08dc33576631c6809b68cfc6a8e832cf8
qiine <qm@asuround.home.lan>
2025-12-22 17:13:43 +0100

    feat(fzf): enable img viewing

--- lua/plugins/ui/fzf-lua.lua
+++ lua/plugins/ui/fzf-lua.lua
@@ -48,6 +48,24 @@ return
                 },
             },
 
+            previewers = {
+              builtin = {
+                extensions = {
+                  -- neovim terminal only supports `viu` block output
+                  -- ["png"] = { "viu", "-b" },
+                  ["png"] = { "ueberzug" },
+                  ["jpg"] = { "ueberzug" },
+                },
+                -- When using 'ueberzug' we can also control the way images
+                -- fill the preview area with ueberzug's image scaler, set to:
+                --   false (no scaling), "crop", "distort", "fit_contain",
+                --   "contain", "forced_cover", "cover"
+                -- For more details see:
+                -- https://github.com/seebye/ueberzug
+                ueberzug_scaler = "fit_contain",
+              }
+            },
+
             fzf_opts = {
                 ["--layout"] = "default",  -- default, reverse (search bar pos)
                 -- ["--marker"] = '>',

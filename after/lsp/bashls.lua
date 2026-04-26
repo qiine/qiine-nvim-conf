a8f96d954bfe13be9312daf011e76b6f9a0cdf09
qiine <qm@asuround.local>
2026-04-26 19:10:20 +0200

    feat: conf some lsps

--- after/lsp/bashls.lua
+++ after/lsp/bashls.lua
@@ -4,7 +4,7 @@ return {
     settings = {
         bashIde = {
             shellcheckArguments = {
-                "-e", "SC2034", -- Hide foo appears unused  https://www.shellcheck.net/wiki/SC2034
+                "-e", "SC2034", -- Hide "foo appears unused"  https://www.shellcheck.net/wiki/SC2034
                 "-e", "SC1090",
                 "-e", "SC1091",
             },
